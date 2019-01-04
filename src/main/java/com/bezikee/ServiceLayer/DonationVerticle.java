35cf7e6e523b027d0e9a5d0e2e48b8fe60d6a60c
Andres Rubio <andresfra92@gmail.com>
2019-01-04 11:46:43 -0400

    Fixed empty db problem

--- src/main/java/com/bezikee/ServiceLayer/DonationVerticle.java
+++ src/main/java/com/bezikee/ServiceLayer/DonationVerticle.java
@@ -141,11 +141,7 @@ public class DonationVerticle extends AbstractVerticle {
             LoggerOps.error("Wrong status: " + request.getParam("status"));
             return true;
         }
-        if ((request.getParam("date") == null) || !(request.getParam("date").matches("^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]" +
-                "))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$"))) {
-            LoggerOps.error("Wrong date: " + request.getParam("date"));
-            return true;
-        }
+
 
 
 
