936eed9e34527b76bdd705386e4ee9f23e45e379
Raul Tuozzo <ratuozzo@gmail.com>
2019-05-07 14:07:38 -0400

    subida

--- src/main/java/com/bezikee/Common/Registry.java
+++ src/main/java/com/bezikee/Common/Registry.java
@@ -3,7 +3,7 @@ package com.bezikee.Common;
 public class Registry {
 
     public static String BD_USER = "raul";
-    public static String BD_PASSWORD = "cachondo1";
+    public static String BD_PASSWORD = "iglesialaboyera2019";
     public static String BD_URL = "jdbc:mysql://localhost:3306/pas";
     public static final String IMG_PATH = "D:\\Repos\\ServicioComunitario\\ParroquiaAsuncionDelSenor\\frontend\\public\\images\\app\\";
 
